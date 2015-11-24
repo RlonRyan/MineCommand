d89a3877f17f6ff249f90689776ddc9f5780a7b4
RlonRyan <rlonryan@aol.com>
2015-11-24 01:25:56 -0700

    Add Proof Of Concept Commands
    Able to implement a few useful commands relatively quickly, without all
    the excess code required by minecraft's vanilla command system.
    
    Note the boolean converter will be internalized.

--- src/main/minecommand/commands/DefaultCommands.java
+++ src/main/minecommand/commands/DefaultCommands.java
@@ -8,9 +8,10 @@ package minecommand.commands;
 import minecommand.MineCommandMod;
 import modcmd.commands.Command;
 import modcmd.commands.CommandParameter;
-import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.WorldSettings;
 
 /**
  *
@@ -20,30 +21,70 @@ public class DefaultCommands {
 
     @Command("broadcast")
     public static void message(@CommandParameter(tag = "m", name = "message", description = "The message to broadcast.", type = "String") String message) {
-        MinecraftServer.getServer().getConfigurationManager().sendChatMsg(new ChatComponentText(message));
+        MinecraftServer.getServer().getConfigurationManager().sendChatMsg(new ChatComponentText("[Broadcast] " + message));
     }
 
     @Command("version")
     public static String getVersion() {
         return MineCommandMod.VERSION;
     }
-
-    @Command("me")
-    public static void me(
-            @CommandParameter(tag = "u", name = "user", description = "The user.", type = "User", defaultValue = "%user%") ICommandSender user,
-            @CommandParameter(tag = "m", name = "message", description = "The message to print.", type = "String") String message) {
-
-        MinecraftServer.getServer().getConfigurationManager().sendChatMsg(new ChatComponentText(user.getCommandSenderName() + " " + message));
-    }
-
-    @Command("here")
-    public static void here(
-            @CommandParameter(tag = "u", name = "user", description = "The user.", type = "User", defaultValue = "%user%") ICommandSender user,
-            @CommandParameter(tag = "x", name = "x-pos", description = "The x-pos.", type = "usrpos", defaultValue = "%") int x,
-            @CommandParameter(tag = "y", name = "y-pos", description = "The y-pos.", type = "usrpos", defaultValue = "%") int y,
-            @CommandParameter(tag = "z", name = "z-pos", description = "The z-pos.", type = "usrpos", defaultValue = "%") int z
+    
+    @Command("gm")
+    public static void gameMode(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user,
+            @CommandParameter(tag = "m", name = "mode", description = "The gamemode.", type = "gamemode", defaultValue = "%") WorldSettings.GameType mode
     ) {
-        user.addChatMessage(new ChatComponentText(String.format("Position: [%1$d,%2$d,%3$d]", x, y, z)));
+        user.setGameType(mode);
+    }
+    
+    @Command("fly")
+    public static void fly(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user,
+            @CommandParameter(tag = "f", name = "fly", description = "If the player can fly.", type = "bool") boolean canFly
+    ) {
+        user.addChatMessage(new ChatComponentText("Old flying ability: " + user.capabilities.allowFlying));
+        user.capabilities.allowFlying = canFly;
+        user.sendPlayerAbilities();
+        user.addChatMessage(new ChatComponentText("Updated flying ability to: " + canFly));
+    }
+    
+    @Command("speed")
+    public static void speed(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user,
+            @CommandParameter(tag = "s", name = "speed", description = "Speed mulitiplier", type = "integer", defaultValue = "1") int speed
+    ) {
+        speed = speed < 1 ? 1 : speed;
+        speed = speed > 10 ? 10 : speed;
+        user.addChatMessage(new ChatComponentText("Old speed: " + user.capabilities.getWalkSpeed() + ", " + user.capabilities.getFlySpeed()));
+        user.capabilities.setPlayerWalkSpeed(0.1f * speed);
+        user.capabilities.setFlySpeed(0.05f * speed);
+        user.sendPlayerAbilities();
+        user.addChatMessage(new ChatComponentText("New speed: " + user.capabilities.getWalkSpeed() + ", " + user.capabilities.getFlySpeed()));
+    }
+    
+    @Command("speed")
+    public static void speed(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user
+    ) {
+        user.addChatMessage(new ChatComponentText("Healing!"));
+        user.heal(20);
+        user.extinguish();
+        user.getFoodStats().addStats(20, 1.0F);
+        user.addChatMessage(new ChatComponentText("Healed!"));
+    }
+    
+    @Command("locate")
+    public static void locate(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user
+    ) {
+        user.addChatMessage(new ChatComponentText(String.format("Location: [%1$d, %2$d, %3$d]", user.chunkCoordX, user.chunkCoordY, user.chunkCoordZ)));
+    }
+    
+    @Command("uuid")
+    public static void uuid(
+            @CommandParameter(tag = "p", name = "player", description = "The target player.", type = "player", defaultValue = "%") EntityPlayer user
+    ) {
+        user.addChatMessage(new ChatComponentText("UUID: " + user.getPersistentID().toString()));
     }
 
 }
--- src/main/minecommand/commands/converters/StaticMineConverters.java
+++ src/main/minecommand/commands/converters/StaticMineConverters.java
@@ -6,16 +6,30 @@
 package minecommand.commands.converters;
 
 import modcmd.converters.Converter;
-import modcmd.converters.ConverterManager;
 import modcmd.converters.exceptions.ConversionException;
 import modcmd.converters.exceptions.ConverterException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.WorldSettings;
 
 /**
  *
  * @author Ryan
  */
 public class StaticMineConverters {
+
+    @Converter("player")
+    public static EntityPlayer getPlayer(Object user, String tag, String value) throws ConverterException {
+        if (value.charAt(0) == '%') {
+            if (user instanceof EntityPlayer) {
+                return (EntityPlayer) user;
+            } else {
+                throw new ConversionException(tag, value, "Entity Player");
+            }
+        } else {
+            throw new ConverterException("Bad user. Alternates not yet supported!");
+        }
+    }
 
     @Converter("usrpos")
     public static int getUserCoordinate(Object user, String tag, String value) throws ConverterException {
@@ -43,6 +57,52 @@ public class StaticMineConverters {
         }
 
         throw new ConversionException(tag, value, "User Coordinate");
+    }
+
+    @Converter("gamemode")
+    public static WorldSettings.GameType getGameType(Object user, String tag, String value) throws ConverterException {
+
+        if (!(user instanceof EntityPlayer)) {
+            throw new ConverterException("Bad user!");
+        }
+
+        EntityPlayer player = (EntityPlayer) user;
+
+        switch (value.charAt(0)) {
+            case '%':
+                return player.capabilities.isCreativeMode ? WorldSettings.GameType.SURVIVAL : WorldSettings.GameType.CREATIVE;
+            case 'S':
+            case 's':
+            case '0':
+                return WorldSettings.GameType.SURVIVAL;
+            case 'C':
+            case 'c':
+            case '1':
+                return WorldSettings.GameType.CREATIVE;
+            case 'A':
+            case 'a':
+            case '2':
+                return WorldSettings.GameType.ADVENTURE;
+        }
+
+        throw new ConversionException(tag, value, "Game Mode");
+    }
+    
+    @Converter("bool")
+    public static boolean getBool(Object user, String tag, String value) throws ConverterException {
+
+        switch (value.charAt(0)) {
+            case 'F':
+            case 'f':
+            case '0':
+                return false;
+            case 'T':
+            case 't':
+            case '1':
+                return true;
+        }
+
+        throw new ConversionException(tag, value, "Boolean");
     }
 
 }
